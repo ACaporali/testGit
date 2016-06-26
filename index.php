03936cc99c8670ab9cfcb502ad4aac38ea7f211d
alice38 <petitkaporal@hotmail.fr>
2016-06-26 16:13:30 +0200

    fusion avec la branche

--- index.php
+++ index.php
@@ -4,12 +4,8 @@
 </head>
 <body>
 	<h1>Accueil</h1>
-<<<<<<< HEAD
-	<p>ajoute de texte pour la branche 1.</p>
-=======
 	<p>ajoute de texte pour la branche 1.</p>
 	<p>encore du texte.</p>
->>>>>>> branch1
 
 </body>
 </html>
